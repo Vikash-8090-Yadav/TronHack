4ac887e7a78bbe075cf93ebe4ec7fc183a5ae55b
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-05-06 17:47:59 +0530

    contract address

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -6,15 +6,22 @@ module.exports = {
   solidity: "0.8.10",
 
   defaultNetwork: "Testnet",
+  // networks:{
+  //   hardhat:{},
+  //   Testnet: {
+  //     chainId: 842,
+  //     url: "https://rpc.testnet.taraxa.io",
+  //     accounts: ["5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702"],
+  //   }
+  // }
   networks:{
     hardhat:{},
-    Testnet: {
-      chainId: 842,
-      url: "https://rpc.testnet.taraxa.io",
-      accounts: ["5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702"],
+    Testnet:{
+      chainId:1029,
+      url:"https://pre-rpc.bt.io",
     }
   }
 };
 
 
-// THis  dapp is deployed on the calibration testnet: 0x8Cf40E23D205dFC19f5A0Ba6bF97722f25EDE856
+// THis  dapp is deployed on the bttc testnet: 0x14f96E014fdb4f0d7F38474f992FB76e759450c1
